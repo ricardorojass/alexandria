17dffac797797ee8d4e924218060cd0f8a0208ed
Ricardo Rojas <reddrum7@gmail.com>
2018-05-20 20:53:20 -0500

    Setup testing environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'factory_bot'
 require 'webmock/rspec'
 require 'database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -53,7 +54,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   # Load Factory Girl methods
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
    # Configure Database Cleaner
    config.before(:suite) do
